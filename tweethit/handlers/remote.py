167df88a1782df80344d9244cf0c3f22b5932c09
O. Can Bascil <ocanbascil@gmail.com>
2011-03-28 23:00:50 +0300

    Added remote memcache delete handler

--- tweethit/handlers/remote.py
+++ tweethit/handlers/remote.py
@@ -10,49 +10,56 @@ QUERY_STRING = "SELECT __key__ FROM "
 helipad.root('tweethit').template_root('static/templates/')
 
 class DeleteHandler(helipad.Handler):
-    def get(self,model_name):
+  def get(self,model_name):
+    if model_name == 'memcache':
+        if memcache.flush_all():
+            self.response.out.write('MEMCACHE FLUSHED')
+            
+    elif model_name == 'template':
+        pythonloader.mydata = {}
+        self.response.out.write('LOCAL TEMPLATE CACHE FLUSHED')
         
-        if model_name == 'memcache':
-            if memcache.flush_all():
-                self.response.out.write('MEMCACHE FLUSHED')
-                
-        elif model_name == 'template':
-            pythonloader.mydata = {}
-            self.response.out.write('LOCAL TEMPLATE CACHE FLUSHED')
+    else:
+        query = db.GqlQuery(QUERY_STRING+model_name)  
+        keys = []
+        for key in query:
+            keys.append(key)
+        pdb.delete(keys)
+        self.response.out.write("Deleted all models of kind: %s" % model_name)
             
-        else:
-            query = db.GqlQuery(QUERY_STRING+model_name)
-            
-            keys = []
-            for key in query:
-                keys.append(key)
-                
-            pdb.delete(keys)
-
-            logging.info("Deleted all models of kind: %s" %model_name)
-            self.response.out.write("Deleted all models of kind: %s" % model_name)
+class DeleteKeyHandler(helipad.Handler):
+  def get(self,model_name,key_name):
+    if model_name == 'memcache':
+      if memcache.delete(key_name):
+        self.response.out.write('% with key name %s deleted from memcache')
+        
+    else:
+      key = db.Key.from_path(model_name,key_name)
+      db.delete(key)
+      self.response.out.write("Deleted %s with key_name: %s" %(model_name,key_name))
 
 class BannedUsers(helipad.Handler):
-    def get(self):
-        users = TwitterUser.all().fetch(10000)
+  def get(self):
+    users = TwitterUser.all().fetch(10000)
 
-        return self.template('banned_model.html', {
-          'users': users,
-        })
-        
-    def post(self):
-        users = self.request.get('users').split('\n')
-        
-        db_targets = []
-        for user_key in users:
-            db_targets.append(TwitterUser(key_name = user_key.strip()))
-        
-        db.put(db_targets)
-        TwitterUser.update_banlist(users)
-        logging.info('Added banned users')
+    return self.template('banned_model.html', {
+      'users': users,
+    })
+      
+  def post(self):
+    users = self.request.get('users').split('\n')
+    
+    db_targets = []
+    for user_key in users:
+      db_targets.append(TwitterUser(key_name = user_key.strip()))
+    
+    db.put(db_targets)
+    TwitterUser.update_banlist(users)
+    logging.info('Added banned users')
                 
 main, application = helipad.app({
     '/remote/delete/(\w+)/': DeleteHandler,
+    '/remote/delete/(\w+)/(\w+)/': DeleteKeyHandler,
     '/remote/bannedusers/':BannedUsers,
 })
 
